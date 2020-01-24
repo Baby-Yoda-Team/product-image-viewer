a3ad34fb22a5a288f6f629d3b9fdb0a619a4f2ec
JoelWukusick <jwukusick@gmail.com>
2020-01-23 23:28:23 -0500

    Connected mongoose

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3004;
 const path = require('path');
+const db = require('../db/index.js');
 
 app.use(express.static(path.join(__dirname, '../public')));
 
