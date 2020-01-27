dc9661c8f4ae8b574130dd8384b50f09ddf63a7e
JoelWukusick <jwukusick@gmail.com>
2020-01-27 14:54:50 -0500

    Create css, style main image

--- src/image.jsx
+++ src/image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Image({ image, product }) {
   return <div>
-    <img src={image + '/300/400.jpg'} alt={product} />
+    <img className='mainImage' src={image + '/1200/1200.jpg'} alt={product} />
   </div>
 }
 
