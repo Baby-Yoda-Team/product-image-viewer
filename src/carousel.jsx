80bde2f73ce851810c63e74d55866f933574d1be
JoelWukusick <jwukusick@gmail.com>
2020-01-28 00:58:47 -0500

    Add variable app width, 100% under 668 px, 50% over 668px

--- src/carousel.jsx
+++ src/carousel.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function Carousel({ images }) {
+  var trackWidth = 20 * images.length;
+  var trackStyle = { width: trackWidth + '%' };
+  var thumbWidth = 100 / images.length;
+  var thumbStyle = { width: thumbWidth + '%' };
   console.log(images);
   return <div className='thumbnails col'>
     <button id='viewPrev' className='arrow fas fa-chevron-left' />
     <div id='slider'>
       <div id='sliderList'>
-        <div id='sliderTrack'>
+        <div id='sliderTrack' style={trackStyle}>
           {images.map(image =>
-            <div className='thumbLink'>
+            <div className='thumbLink' style={thumbStyle}>
               <img id={image} className='thumbnail' src={image + '/200/200.jpg'} />
             </div>
           )}
