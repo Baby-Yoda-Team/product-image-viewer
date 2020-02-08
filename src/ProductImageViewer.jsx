90c42e797d2de3d586e5d1c0c5882d140bd010dc
JoelWukusick <jwukusick@gmail.com>
2020-02-08 15:49:49 -0500

    Hard code amazon URL

--- src/ProductImageViewer.jsx
+++ src/ProductImageViewer.jsx
@@ -6,7 +6,7 @@ import Image from './image.jsx';
 import Carousel from './carousel.jsx';
 
 const port = 3004;
-var url = `http://${window.location.hostname}:${port}`;
+var url = `http://http://ec2-3-14-128-79.us-east-2.compute.amazonaws.com:${port}`;
 var params = (new URLSearchParams(window.location.search));
 
 class ProductImageViewer extends React.Component {
